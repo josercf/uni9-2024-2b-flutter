edb01c970e75e67b009c5971aa1f649eed642288
José Romualdo <joseromualdo@outlook.com>
2024-08-14 11:32:24 -0300

    feat: adicionando arquivos aula 2

---
+++ Readme.md
@@ -0,0 +1,12 @@
+Conteúdo da aula 2, introdução ao DART
+
+
+Comandos para a criação do projeto:
+
+```
+$ dart create -t console my_app
+$ cd my_app
+$ dart analyze
+$ dart test
+$ dart run bin/my_app.dart
+```
---
+++ main.dart
@@ -0,0 +1,20 @@
+void main() {
+  String nome = 'José Romualdo';
+  print('Olá, ${nome}');
+  
+  
+  nome = 'Sr. Costa';
+  print('Olá, ${nome}');
+  
+  /*
+  bool estaChovendo = true;
+  print('Está chovendo? $estaChovendo');
+  */
+  
+  bool estaChovendo = true;
+  print("está chovendo? $estaChovendo");
+  
+  var idade = 31;
+  
+  print("O ${nome} possui ${idade} anos.");
+}
