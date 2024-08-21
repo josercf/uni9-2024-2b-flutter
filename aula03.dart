c1b90c73dc3e46a1a384ff73c9fef6e13720fb3f
José Romualdo <joseromualdo@outlook.com>
2024-08-21 09:56:16 -0300

    feat: else if  statement

--- aula03.dart
+++ aula03.dart
@@ -2,9 +2,16 @@ void main() {
   double nota = 7;
   int faltas = 11;
 
+  
+  //< 5: REPROVADO
+  //Entre 5 e 7 RECUPERAÇÃO
+  //> 7: Aprovado
+  
   if (nota < 5) {
     print("Reprovado");
-  } else {
+  } else if(nota < 7){
+    print("Recuperação");
+  }  else {
     print("Aprovado");
   }
 
