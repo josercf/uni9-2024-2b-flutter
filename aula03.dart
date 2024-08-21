e15f4b8fe0175965fcc21a558c1d897972724afd
José Romualdo <joseromualdo@outlook.com>
2024-08-21 10:10:56 -0300

    feat: tenary  statement

--- aula03.dart
+++ aula03.dart
@@ -2,18 +2,22 @@ void main() {
   double nota = 7;
   int faltas = 11;
 
-  
   //< 5: REPROVADO
   //Entre 5 e 7 RECUPERAÇÃO
   //> 7: Aprovado
-  
+
   if (nota < 5) {
     print("Reprovado");
-  } else if(nota < 7){
+  } else if (nota < 7) {
     print("Recuperação");
-  }  else {
+  } else {
     print("Aprovado");
   }
+
+  //Exemplo de ternário
+
+  String info = nota >= 7 && faltas <= 10 ? "Aprovado" : "Reprovado";
+  print("Resultado do ternário: $info");
 
   print("Fim do processamento");
 }
